e12a8045a05d65856591b02b38f3e1f931704532
Navaneeth Malingan <navneetnivu07@gmail.com>
2020-07-21 10:07:03 +0530

    iris classificaiton

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ model = pickle.load(open('model.pkl', 'rb'))
 clf = pickle.load(open('nlp_model.pkl', 'rb'))
 cv = pickle.load(open('tranform.pkl', 'rb'))
 
+# iris classification
+
+iris_model = pickle.load(open('models/iris-model.pkl', 'rb'))
+iris_target = pickle.load(open('models/target_names.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
@@ -22,6 +27,34 @@ def home():
 @app.route('/email')
 def email():
     return render_template('home.html')
+
+
+@app.route('/iris', methods=['POST'])
+def iris_predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    format = request.args.get('format')
+    print("here", format)
+    print("new line")
+
+    # experience = request.form['experience']
+    # petal_len = request.form['pl]
+    # sl
+    # ip_Ary = [pl, sl, pwm sw]
+
+    int_features = [float(x) for x in request.form.values()]
+    print(int_features)
+    final_features = [np.array(int_features)]
+    prediction = iris_model.predict(final_features)
+
+    output = prediction[0]
+    target_name = iris_target[output]
+
+    if(format == 'json'):
+        return jsonify({'output': int(output), 'target_name': target_name})
+
+    return render_template('index.html', prediction_text='Target class is  {}'.format(output))
 
 
 @app.route('/predict', methods=['POST'])
